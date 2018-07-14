69838c90d31c728271e457815a9ea61e94b5658f
yogiwisesa <yogiwisesa@gmail.com>
2018-07-14 19:18:52 +0700

    PutExtra di login, get Extra di Menu Kader, SetContentView di Menu Kader

--- app/src/main/java/idev/gin/nias/activity/LoginActivity.java
+++ app/src/main/java/idev/gin/nias/activity/LoginActivity.java
@@ -47,7 +47,7 @@ import static idev.gin.nias.data.remote.RetrofitClient.retrofit;
  */
 public class LoginActivity extends AppCompatActivity {
     // TODO: Usahain pake variabel global secukupnya aja. Lebih baik pake variabel lokal.
-    private UserLoginTask mAuthTask = null;
+//    private UserLoginTask mAuthTask = null;
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
@@ -58,19 +58,18 @@ public class LoginActivity extends AppCompatActivity {
     /**
      * Kalo bingung, dao dipisah aja setiap endpointnya
      * Dao masih kotlin, ubah aja ke java kl maum gua gada pojo converter
-     *
+     * <p>
      * Login dikasih overlay item aga transparan, ga keliatan soalnya teksnya
-     *
+     * <p>
      * Dokumentasi dan penggunaan FAN: https://github.com/amitshekhariitbhu/Fast-Android-Networking
      * Pake yang getAsObject jangan yang JSON, biar rapih dan ga bingung, tapi kalo mau juga gapapa.
-     *
+     * <p>
      * Refrensi clean code: https://github.com/ryanmcdermott/clean-code-javascript
-     *
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setBackgroundDrawableResource(R.drawable.bg);
+//        getWindow().setBackgroundDrawableResource(R.drawable.bg);
         setContentView(R.layout.activity_login);
         // Set up the login form.
         mSignUpButton = (Button) findViewById(R.id.singup);
@@ -89,7 +88,7 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
                 if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
+//                    attemptLogin();
                     return true;
                 }
                 return false;
@@ -122,7 +121,7 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(LoginDao response) {
                         String token = response.getToken();
-                        getAkunId(email, response.getToken());
+                        getAkunId(email, token);
                     }
 
                     @Override
@@ -132,7 +131,7 @@ public class LoginActivity extends AppCompatActivity {
                 });
     }
 
-    public void getAkunId(String email, String token) {
+    public void getAkunId(final String email, final String token) {
         AndroidNetworking.get(CONSTANT.BASE_URL + "akunid")
                 .addHeaders("Authorization", "bearer " + token)
                 .addHeaders("email", email)
@@ -143,84 +142,99 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(AkunidDao response) {
                         String role = response.getResult().get(0).getRole();
-                        Log.i("role",role);
-                        if(role.contains("Kader")){
-                            Toast.makeText(getApplicationContext(), "KADER", Toast.LENGTH_LONG).show();
-                                startkader();
-                            }
-                            else if(role.contains("Nakes")) {
-                            Toast.makeText(getApplicationContext(), "NAKES", Toast.LENGTH_LONG).show();
-                            startnakes();
+                        Log.i("role", role);
 
-                            }
-                            else {
-                            Toast.makeText(getApplicationContext(), "Role not found", Toast.LENGTH_LONG).show();
+                        // Cek kondisi login berhasil
+                        Intent i;
+                        if (role.toUpperCase().contains("KADER")){
+                            i = new Intent(LoginActivity.this, MenuKaderActivity.class);
+                        } else if (role.toUpperCase().contains("NAKES")){
+                            i = new Intent(LoginActivity.this, MenuNakesActivity.class);
+                        } else {
+                            return;
                         }
+                        i.putExtra("email", email);
+                        i.putExtra("token", token);
+                        Log.i("xxx", "belum startactivity");
+                        startActivity(i);
+                        Log.i("xxx", "Sudah startactivity");
+
+
+//                        if (role.contains("Kader")) {
+//                            Toast.makeText(getApplicationContext(), "KADER", Toast.LENGTH_LONG).show();
+//                            startkader();
+//                        } else if (role.contains("Nakes")) {
+//                            Toast.makeText(getApplicationContext(), "NAKES", Toast.LENGTH_LONG).show();
+//                            startnakes();
+//
+//                        } else {
+//                            Toast.makeText(getApplicationContext(), "Role not found", Toast.LENGTH_LONG).show();
+//                        }
                     }
 
-                    private void startkader() {
-                        Intent intent = new Intent(LoginActivity.this,MenuKaderActivity.class);
-                        startActivity(intent);
-                    }
-
-                    private void startnakes() {
-                        Intent intent = new Intent(LoginActivity.this,MenuNakesActivity.class);
-                        startActivity(intent);
-                    }
+//                    private void startkader() {
+//                        Intent intent = new Intent(LoginActivity.this, MenuKaderActivity.class);
+//                        startActivity(intent);
+//                    }
+//
+//                    private void startnakes() {
+//                        Intent intent = new Intent(LoginActivity.this, MenuNakesActivity.class);
+//                        startActivity(intent);
+//                    }
 
                     @Override
                     public void onError(ANError anError) {
-                        Toast.makeText(getApplicationContext(),  "Error: " + anError.getErrorBody(), Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Error: " + anError.getErrorBody(), Toast.LENGTH_LONG).show();
                     }
                 });
     }
 
 
-    private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
-        // Reset errors.
-        mEmailView.setError(null);
-        mPasswordView.setError(null);
-
-
-        // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
-        String password = mPasswordView.getText().toString();
-
-
-        boolean cancel = false;
-        View focusView = null;
-
-        // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-
-        // Check for a valid email address.
-        if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
-            cancel = true;
-        } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_email));
-            focusView = mEmailView;
-            cancel = true;
-        }
-
-        if (cancel) {
-            focusView.requestFocus();
-        } else {
-            showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
-        }
-
-    }
+//    private void attemptLogin() {
+//        if (mAuthTask != null) {
+//            return;
+//        }
+//
+//        // Reset errors.
+//        mEmailView.setError(null);
+//        mPasswordView.setError(null);
+//
+//
+//        // Store values at the time of the login attempt.
+//        String email = mEmailView.getText().toString();
+//        String password = mPasswordView.getText().toString();
+//
+//
+//        boolean cancel = false;
+//        View focusView = null;
+//
+//        // Check for a valid password, if the user entered one.
+//        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+//            mPasswordView.setError(getString(R.string.error_invalid_password));
+//            focusView = mPasswordView;
+//            cancel = true;
+//        }
+//
+//        // Check for a valid email address.
+//        if (TextUtils.isEmpty(email)) {
+//            mEmailView.setError(getString(R.string.error_field_required));
+//            focusView = mEmailView;
+//            cancel = true;
+//        } else if (!isEmailValid(email)) {
+//            mEmailView.setError(getString(R.string.error_invalid_email));
+//            focusView = mEmailView;
+//            cancel = true;
+//        }
+//
+//        if (cancel) {
+//            focusView.requestFocus();
+//        } else {
+//            showProgress(true);
+//            mAuthTask = new UserLoginTask(email, password);
+//            mAuthTask.execute((Void) null);
+//        }
+//
+//    }
 
     private boolean isEmailValid(String email) {
         return email.contains("@");
@@ -265,54 +279,54 @@ public class LoginActivity extends AppCompatActivity {
             mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
         }
     }
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            APIServiceLogin loginAPI = retrofit.create(APIServiceLogin.class);
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
-    }
 }
+//    /**
+//     * Represents an asynchronous login/registration task used to authenticate
+//     * the user.
+//     */
+//    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+//
+//        private final String mEmail;
+//        private final String mPassword;
+//
+//        UserLoginTask(String email, String password) {
+//            mEmail = email;
+//            mPassword = password;
+//        }
+//
+//        @Override
+//        protected Boolean doInBackground(Void... params) {
+//            APIServiceLogin loginAPI = retrofit.create(APIServiceLogin.class);
+//
+//            try {
+//                // Simulate network access.
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                return false;
+//            }
+//
+//            // TODO: register the new account here.
+//            return true;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(final Boolean success) {
+//            mAuthTask = null;
+//            showProgress(false);
+//
+//            if (success) {
+//                finish();
+//            } else {
+//                mPasswordView.setError(getString(R.string.error_incorrect_password));
+//                mPasswordView.requestFocus();
+//            }
+//        }
+//
+//        @Override
+//        protected void onCancelled() {
+//            mAuthTask = null;
+//            showProgress(false);
+//        }
+//    }
+//}
 
